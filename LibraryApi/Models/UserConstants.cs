57d673f360bbe05606def0ea589e7a4b38679175
jacquilinegenato_domain <jacquiline.genato@domain.com.au>
2024-10-31 20:50:07 +0800

    Initial commit
    - Login
    - Authentication

---
+++ LibraryApi/Models/UserConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryApi.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "jacgenato", EmailAddress="jac_genato@gmail.com", Password="Password",
+                GivenName="Jac", Surname="Genato", Role="Administrator"},
+            new UserModel() { Username = "jobeekevin", EmailAddress="jobee@gmail.com", Password="Password",
+                GivenName="Kevin", Surname="Ellamil", Role="Seller"},
+        };
+    }
+}
